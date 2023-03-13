5f320c2494a11ba93d941d50d34bce4ea6936500
mathiasg12 <mathiasgausl@gmail.com>
2023-03-13 19:47:34 +0100

    v15.2
    working cart

--- js/cart.js
+++ js/cart.js
@@ -11,14 +11,14 @@ let filteredContent= content.filter( items=> {return items !== null})
 function displayContent(){
        for(let i=0; i < filteredContent.length; i++ ){
         let parsedContent=JSON.parse(filteredContent[i])
-        cart_content.innerHTML += `<div class="cart_items"><h2>Name: ${parsedContent.name}</h2><p>Price: ${parsedContent.price}$</p><button class= "remove">-</button></div>`
+        cart_content.innerHTML += `<div class="cart_items"><h2>Name: ${parsedContent.name}</h2><p>Price: ${parsedContent.price}$</div>`
+        let remove_btn= document.createElement("button");
+        remove_btn.innerHTML += "-";
+        cart_content.append(remove_btn)
         sum += parsedContent.price;
-        totall.innerHTML= sum;
+        totall.innerHTML= sum + "$";
     }}
  
-
-
-
 if(localStorage.length >= 1){
     displayContent()
 }
