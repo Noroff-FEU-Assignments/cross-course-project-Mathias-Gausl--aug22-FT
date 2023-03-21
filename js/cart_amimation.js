a3973abfd7395ab506eeccc006ead963bfe5947e
mathiasg12 <mathiasgausl@gmail.com>
2023-03-21 18:39:12 +0100

    v16.5
    fixed nav

--- js/cart_amimation.js
+++ js/cart_amimation.js
@@ -1,4 +1,5 @@
 const cart = document.querySelector("#cart");
+const cartMob = document.querySelector("#cartMobile");
 const buyBtn = document.querySelector(".buy")
 const ham = document.querySelector("#checkbox");
 const nav = document.querySelector("nav");
@@ -11,12 +12,19 @@ let num = localStorage.length;
 function newItem() {
   if (localStorage.length ++) {
     cart.innerHTML += `<em  class="itemAdded">${num}</em>`;
-    cart.style.fontSize = "1.3em";
+    cart.style.fontSize = "1.5em";
+    cartMob.innerHTML += `<em  class="itemAdded">${num}</em>`;
+    cartMob.style.fontSize = "1.3em";
 
     
   } else {
     cart.innerHTML = ` 
       <a href="cart.html" id="cart">
+        <i class="fa-solid fa-cart-shopping"></i>
+      </a>
+    `;
+    cartMob.innerHTML = ` 
+      <a href="cart.html" id="cartMobile">
         <i class="fa-solid fa-cart-shopping"></i>
       </a>
     `;
