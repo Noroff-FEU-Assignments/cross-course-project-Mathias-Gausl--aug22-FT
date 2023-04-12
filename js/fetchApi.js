af27c7724720be545b8a1951d160ab25b63f19b8
mathiasg12 <mathiasgausl@gmail.com>
2023-04-12 16:51:33 +0200

    v16
    started details, finnshed index

--- js/fetchApi.js
+++ js/fetchApi.js
@@ -1,4 +1,6 @@
 const url = "http://gamehubproducts.local/wp-json/wc/store/products";
+const loading = document.querySelector(".loading");
+const card = document.querySelector(".slide");
 let getfunction = async function getGames(categ, con) {
   try {
     const fetchapi = await fetch(url);
@@ -9,26 +11,43 @@ let getfunction = async function getGames(categ, con) {
       console.log(objects);
       for (let i = 0; i < objects.categories.length; i++) {
         let categori = objects.categories[i];
+        let image = objects.images[0];
         console.log(categori.name);
         if (objects.on_sale === true) {
           if (categori.name === categ) {
-            con.innerHTML += `<a><h2>${objects.name}</h2><h3>${
+            loading.innerHTML = "";
+            loading.style.margin = "0";
+            con.innerHTML += `<a class="slide" href="gameDetails.html?id=${
+              objects.id
+            }"><img src="${image.src}" alt="${
+              objects.name
+            } game cover"</img><h2>${objects.name}</h2><h3>${
               objects.description
-            }</h3><p>${objects.prices.regular_price / 100}£${
+            }</h3><em class= "oldPrice">${
+              objects.prices.regular_price / 100
+            }£</em><em class="currentPrice">${
               objects.prices.price / 100
-            }£</p></a>`;
+            }£</em></a>`;
           }
         } else {
           if (categori.name === categ) {
-            con.innerHTML += `<a><h2>${objects.name}</h2><h3>${
+            loading.innerHTML = "";
+            loading.style.margin = "0";
+            con.innerHTML += `<a class="slide" href="gameDetails.html?id=${
+              objects.id
+            }"><img src="${image.src}" alt="${objects.name} game cover"><h2>${
+              objects.name
+            }</h2></img><h3>${
               objects.description
-            }</h3><p>${objects.prices.price / 100}£</p></a>`;
+            }</h3><em class="currentPrice">${
+              objects.prices.price / 100
+            }£</em></a>`;
           }
         }
       }
     }
   } catch (error) {
-    console.log("error");
+    console.log(error);
   }
 };
 export default getfunction;
