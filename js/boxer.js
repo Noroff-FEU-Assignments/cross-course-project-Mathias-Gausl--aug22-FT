6e45bd798ef96cde615a7831dd10ddb18f814eed
mathiasg12 <mathiasgausl@gmail.com>
2023-10-26 14:39:05 +0200

    added auto fix on save

--- js/boxer.js
+++ js/boxer.js
@@ -1,7 +1,9 @@
-const boxer_btn= document.querySelector("#boxer_btn");
-const opt= document.querySelector("select")
-function addToCart(){
- const boxerObj ={name: "Boxer", price: 20,id: 3, console: opt.value};
- localStorage.setItem("shoppingcart3", JSON.stringify(boxerObj));
+const boxer_btn = document.querySelector("#boxer_btn");
+const opt = document.querySelector("select");
+function addToCart() {
+  const boxerObj = { name: "Boxer", price: 20, id: 3, console: opt.value };
+  localStorage.setItem("shoppingcart3", JSON.stringify(boxerObj));
 }
-boxer_btn.addEventListener("click", ()=>{addToCart()})
+boxer_btn.addEventListener("click", () => {
+  addToCart();
+});
--- js/racing.js
+++ js/racing.js
@@ -1,6 +1,8 @@
-const racing_btn= document.querySelector("#racing_btn");
-function addToCart(){
- const racingObj ={name: "Racing", price: 20,id: 6, console: "PS4" };
- localStorage.setItem("shoppingcart6", JSON.stringify(racingObj));
+const racing_btn = document.querySelector("#racing_btn");
+function addToCart() {
+  const racingObj = { name: "Racing", price: 20, id: 6, console: "PS4" };
+  localStorage.setItem("shoppingcart6", JSON.stringify(racingObj));
 }
-racing_btn.addEventListener("click", ()=>{addToCart()})
+racing_btn.addEventListener("click", () => {
+  addToCart();
+});
