f79a1a88040aa7ec74534dbdf361f4af10664c13
mathiasg12 <mathiasgausl@gmail.com>
2023-10-28 19:38:41 +0200

    fixed local server had to remove elsint dix

--- cypress/e2e/4-login/login.cy.js
+++ cypress/e2e/4-login/login.cy.js
@@ -1,11 +1,11 @@
-describe("go to login from landing page and login",function(){
-    it("goes to login page",function(){
-        cy.visit("http://127.0.0.1:5500/index.html")
-        cy.get('ul > :nth-child(3) > a').click().wait(2000)
-    })
-    it("fill out login information",function(){
-        cy.get('#em').type("ddsjdsj@gmail.com").wait(2000)
-        cy.get('#pass').type("ddsjdsjwwwwwwww").wait(2000)
-        cy.get('#submit').click().wait(2000)
-    })
-})
+describe("go to login from landing page and login", function () {
+  it("goes to login page", function () {
+    cy.visit("/index.html");
+    cy.get("ul > :nth-child(3) > a").click().wait(2000);
+  });
+  it("fill out login information", function () {
+    cy.get("#em").type("ddsjdsj@gmail.com").wait(2000);
+    cy.get("#pass").type("ddsjdsjwwwwwwww").wait(2000);
+    cy.get("#submit").click().wait(2000);
+  });
+});
